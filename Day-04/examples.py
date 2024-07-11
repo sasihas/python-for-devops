7248e1546deebb79ba6153f2e7ef60680464e47e
sasihas <60338548+sasihas@users.noreply.github.com>
2024-07-11 20:40:34 +0530

    Create examples.py

---
+++ Day-04/examples.py
@@ -0,0 +1,23 @@
+a = 10
+b = 3
+
+def addition():
+    add = a + b
+    print(add)
+
+def sub():
+    s = abs(a - b)
+    print(s)
+
+def multiplication():
+    mul = a * b
+    print(mul)
+
+def division():
+    divi = round(a / b)
+    print(divi)
+
+addition()
+sub()
+multiplication()
+division()
